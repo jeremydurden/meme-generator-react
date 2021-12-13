2f8f1272700fa1862faa9a8dcb2971cea5fc5be0
Jeremy Durden <jeremydurden@gmail.com>
2021-12-13 14:02:18 -0800

    refactor state for meme component

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -3,11 +3,20 @@ import { useState } from "react";
 import memesData from "../memesData";
 
 function Meme() {
-  const [memeImage, setMemeImage] = useState("");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
 
   function getMemeImage() {
-    let random = Math.floor(Math.random() * memesData.data.memes.length);
-    setMemeImage(memesData.data.memes[random].url);
+    let random = Math.floor(Math.random() * allMemeImages.data.memes.length);
+    setMeme((prevState) => ({
+      ...prevState,
+      randomImage: allMemeImages.data.memes[random].url,
+    }));
   }
   return (
     <main className="meme--container">
@@ -24,7 +33,7 @@ function Meme() {
           </button>
         </div>
       </div>
-      <img className="meme--image" src={`${memeImage}`} alt="" />
+      <img className="meme--image" src={`${meme.randomImage}`} alt="" />
     </main>
   );
 }
@@ -32,12 +41,18 @@ function Meme() {
 export default Meme;
 
 /**
- * Challenge: Save the random meme URL in state
- * - Create new state called `memeImage` with an
- *   empty string as default
- * - When the getMemeImage function is called, update
- *   the `memeImage` state to be the random chosen
- *   image URL
- * - Below the div.form, add an <img /> and set the
- *   src to the new `memeImage` state you created
+ * Challenge: Update our state to save the meme-related
+ * data as an object called `meme`. It should have the
+ * following 3 properties:
+ * topText, bottomText, randomImage.
+ *
+ * The 2 text states can default to empty strings for now,
+ * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+ *
+ * Next, create a new state variable called `allMemeImages`
+ * which will default to `memesData`, which we imported above
+ *
+ * Lastly, update the `getMemeImage` function and the markup
+ * to reflect our newly reformed state object and array in the
+ * correct way.
  */
